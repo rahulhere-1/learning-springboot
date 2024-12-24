12eabe92b157e04de4811e051b68518ae1f47369
rahulhere-1 <rmendigeri4@gmail.com>
2024-12-24 22:49:24 +0530

    revised endpoints access and changed port number for server

--- src/main/java/com/example/learning/Learning/Spring/Boot/ApplicationSecurityConfig.java
+++ src/main/java/com/example/learning/Learning/Spring/Boot/ApplicationSecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.learning.Learning.Spring.Boot;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,7 +14,9 @@ public class ApplicationSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .authorizeHttpRequests( auth -> auth.requestMatchers("/api/**").permitAll())
+                .authorizeHttpRequests( auth -> auth.requestMatchers("/api/**").authenticated())
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults())
                 .build();
     }
 }
